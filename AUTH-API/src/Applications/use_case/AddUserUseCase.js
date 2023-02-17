69c2a4b24f27efdd1a9d02579aabe038b7348dab
hikio-17 <fajri010217@gmail.com>
2023-02-16 21:00:38 -0800

    create addUserCase and PasswordHash

---
+++ AUTH-API/src/Applications/use_case/AddUserUseCase.js
@@ -0,0 +1,17 @@
+const RegisterUser = require('../../Domains/users/entities/RegisterUser');
+
+class AddUserCase {
+   constructor({ userRepository, passwordHash }) {
+      this._userRespository = userRepository;
+      this._passwordHash = passwordHash;
+   }
+
+   async execute(useCasePayload) {
+      const registerUser = new RegisterUser(useCasePayload);
+      await this._userRespository.verifyAvailableUsername(useCasePayload.username);
+      registerUser.password = await this._passwordHash.hash(registerUser.password);
+      return this._userRespository.addUser(registerUser);
+   }
+}
+
+module.exports = AddUserCase;
